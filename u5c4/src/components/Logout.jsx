3451436453b27815bf7ea0dff840c8d618e12a7a
Vaibhav Girish Ginnalwar <97450767+Vaibhav0702@users.noreply.github.com>
2022-05-02 16:59:57 +0530

    u5c4

---
+++ u5c4/src/components/Logout.jsx
@@ -0,0 +1,21 @@
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { logout } from '../Redux/actions';
+// import { store } from '../Redux/store';
+import { Navigate } from "react-router-dom";
+
+export const Logout = () => {
+    // Logout component, just log user out and take him to `/` homepage
+
+    // suggestion: if you are storing anyting in redux it's a good idea to
+    // empty it before loggin out. eg: order
+    const dispatch = useDispatch();
+    useSelector((store) => {
+        console.log(store);
+        return store.username;
+    });
+
+    dispatch(logout('logout'))
+
+    return <><Navigate to="/" /></>;
+};
